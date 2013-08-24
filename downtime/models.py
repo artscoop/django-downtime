7f72cde0babd7d091a7990b40d53aa8a1e1d3db4
artscoop <artscoop93.info@gmail.com>
2013-08-24 22:23:22 +0200

    Update models.py
    Made model definition more decent for the admin.

--- downtime/models.py
+++ downtime/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from downtime.managers import PeriodManager
+from django.utils.translation import ugettext, ugettext_lazy as _
+
 
 class Period(models.Model):
-    start_time = models.DateTimeField(blank=True, null=True)
-    end_time = models.DateTimeField(blank=True, null=True)
-    enabled = models.BooleanField()
+    start_time = models.DateTimeField(blank=True, null=True, verbose_name=_(u"Start"))
+    end_time = models.DateTimeField(blank=True, null=True, verbose_name=_(u"End"))
+    enabled = models.BooleanField(default=True, verbose_name=_(u"Enabled"))
 
     objects = PeriodManager()
 
     def __unicode__(self):
-        return "Scheduled downtime"
+        return ugettext(u"scheduled downtime")
+    
+    class Meta:
+        verbose_name = _(u"scheduled downtime")
+        verbose_name_plural = _(u"scheduled downtimes")
