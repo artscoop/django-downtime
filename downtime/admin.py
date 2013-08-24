b4ee6893eb83547ccd223b63dc3bff18139734b3
artscoop <artscoop93.info@gmail.com>
2013-08-24 22:27:12 +0200

    Update admin.py
    customized admin for downtime periods.

--- downtime/admin.py
+++ downtime/admin.py
@@ -1,4 +1,14 @@
 from downtime.models import Period
 from django.contrib import admin
 
-admin.site.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    # Scheduled downtime admin configuration
+    list_select_related = True
+    list_display = ['id', 'enabled', 'start_time', 'end_time']
+    list_filter = ['enabled']
+    search_fields = []
+    actions = []
+    save_on_top = False
+    actions_on_top = True
+
+admin.site.register(Period, PeriodAdmin)
